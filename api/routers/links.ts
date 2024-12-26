ea6457489e9f61e83b28d82e02c7e3ab0185814f
Oleg <evolrn89@gmail.com>
2024-12-26 11:25:40 +0600

    [API] - created URL shortener API with unique shortUrl generation and error handling

--- api/routers/links.ts
+++ api/routers/links.ts
@@ -1,26 +1,64 @@
 import express from "express";
+import Link from "../models/Link";
+import {customAlphabet} from "nanoid";
+import isUrl from "is-url";
 
 const linksRouter = express.Router();
 
-linksRouter.get('/', async (_req, res) => {
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+linksRouter.get('/', async (_req, res, next) => {
     try {
-    } catch {
-        res.sendStatus(500);
+        const results = await Link.find();
+        res.send(results);
+    } catch (e) {
+        next(e);
     }
 });
 
-linksRouter.get('/:id', async (req, res) => {
+linksRouter.get('/:shortUrl', async (req, res, next) => {
     try {
-    } catch {
-        res.sendStatus(500);
+        const result = await Link.findOne({ shortUrl: req.params.shortUrl });
+        if (!result) {
+            res.status(404).json({ error: 'Not found' });
+            return;
+        }
+        res.status(301).redirect(result.originalUrl);
+    } catch (e) {
+        next(e);
     }
 });
 
-linksRouter.post('/', async (req, res) => {
+linksRouter.post('/', async (req, res, next) => {
+    const { url } = req.body;
 
-    try {
-    } catch (error) {
-        res.status(400).send(error);
+    if (!url) {
+        res.status(400).json({ error: 'URL is required' });
+        return;
+    } else if (!isUrl(url)) {
+        res.status(400).json({ error: 'Invalid URL' });
+        return;
+    }
+
+    const nanoid = customAlphabet(alphabet, 7);
+    const shortUrl = nanoid();
+
+    if (await Link.findOne({ shortUrl })) {
+        res.status(400).json({ error: 'An error occurred while generating short url, short url already exists' });
+        return;
+    } else {
+        const linkData = {
+            originalUrl: url,
+            shortUrl
+        };
+
+        const link = new Link(linkData);
+        try {
+            await link.save();
+            res.status(200).send(link);
+        } catch (e) {
+            next(e);
+        }
     }
 });
 
---
+++ api/type.d.ts
@@ -0,0 +1,4 @@
+export interface ILink {
+    shortUrl: string;
+    originalUrl: string;
+}
